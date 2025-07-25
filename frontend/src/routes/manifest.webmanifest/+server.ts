a758798912ffef32c281a11dabdfd185239a435c
James18232 <jjccargill@gmail.com>
2025-07-25 23:18:26 +1000

    Update +server.ts

--- frontend/src/routes/manifest.webmanifest/+server.ts
+++ frontend/src/routes/manifest.webmanifest/+server.ts
@@ -2,24 +2,24 @@ import type { RequestHandler } from './$types';
 import { getAppleIconUrl } from '$lib/utils/apple-icon-util';
 import type { RequestEvent } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
-	const logoUrl = getAppleIconUrl();
-	const manifest = {
-		name: 'PocketID',
-		icons: [
-			{
-				src: logoUrl,
-				sizes: 'any'
-			}
-		],
-		display: 'browser',
-		background_color: '#000000',
-		theme_color: '#000000'
-	};
+export async function GET({ request }: RequestEvent) {
+  const logoUrl = getAppleIconUrl();
+  const manifest = {
+    name: "PocketID",
+    icons: [
+      {
+        src: logoUrl
+      }
+    ],
+    display: "browser",
+    background_color: "#000000",
+    theme_color: "#000000"
+  };
 
-	return new Response(JSON.stringify(manifest), {
-		headers: {
-			'Content-Type': 'application/manifest+json'
-		}
-	});
-};
+  return new Response(JSON.stringify(manifest), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+
