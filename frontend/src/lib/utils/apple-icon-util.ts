9d00a39e192b910c67705862f884320390e2df12
James18232 <jjccargill@gmail.com>
2025-07-25 22:33:11 +1000

    Create apple-icon-util.ts

---
+++ frontend/src/lib/utils/apple-icon-util.ts
@@ -0,0 +1,7 @@
+import { mode } from 'mode-watcher';
+import { cachedApplicationLogo } from '$lib/utils/cached-image-util';
+
+export function getAppleIconUrl() {
+	const isLightMode = mode.current === 'light';
+	return cachedApplicationAppleIcon.getUrl(isLightMode);
+}
