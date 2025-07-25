2d54e55e8477395d08977a7c58bfd9aec8685c2c
James18232 <jjccargill@gmail.com>
2025-07-25 22:47:54 +1000

    Update app_config_controller.go

--- backend/internal/controller/app_config_controller.go
+++ backend/internal/controller/app_config_controller.go
@@ -35,6 +35,7 @@ func NewAppConfigController(
 	group.PUT("/application-configuration", authMiddleware.Add(), acc.updateAppConfigHandler)
 
 	group.GET("/application-configuration/logo", acc.getLogoHandler)
+	group.GET("/application-configuration/AppleIcon", acc.getAppleIconHandler)
 	group.GET("/application-configuration/background-image", acc.getBackgroundImageHandler)
 	group.GET("/application-configuration/favicon", acc.getFaviconHandler)
 	group.PUT("/application-configuration/logo", authMiddleware.Add(), acc.updateLogoHandler)
@@ -155,6 +156,31 @@ func (acc *AppConfigController) getLogoHandler(c *gin.Context) {
 
 	acc.getImage(c, imageName, imageType)
 }
+
+// getAppleIconHandler godoc
+// @Summary Get apple touch icon
+// @Description Get the apple touch icon for the application
+// @Tags Application Configuration
+// @Param light query boolean false "Light mode logo (true) or dark mode logo (false)"
+// @Produce image/png
+// @Success 200 {file} binary "Logo image"
+// @Router /api/application-configuration/AppleIcon [get]
+func (acc *AppConfigController) getAppleIconHandler(c *gin.Context) {
+
+	lightLogo, _ := strconv.ParseBool(c.DefaultQuery("light", "true"))
+
+	var imageName, imageType string
+	if lightLogo {
+		imageName = "AppleTouchIconLight"
+		imageType = "png"
+	} else {
+		imageName = "AppleTouchIconDark"
+		imageType = "png"
+	}
+
+	acc.getImage(c, imageName, imageType)
+}
+
 
 // getFaviconHandler godoc
 // @Summary Get favicon
