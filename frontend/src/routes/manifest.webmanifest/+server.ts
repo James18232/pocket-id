b3038170ff482c24a34e80b39604b945e5a9d388
James18232 <jjccargill@gmail.com>
2025-07-26 00:31:57 +1000

    Update +server.ts

--- frontend/src/routes/manifest.webmanifest/+server.ts
+++ frontend/src/routes/manifest.webmanifest/+server.ts
@@ -1,1 +1,23 @@
+import type { RequestHandler } from './$types';
+export const prerender = true; // this should just prerender manifest.json and not treat it as a server route
 
+export const GET: RequestHandler = async () => {
+	const manifest = {
+		name: 'PocketID',
+		icons: [
+			{
+				src: '/api/application-configuration/logo',
+				sizes: 'any'
+			}
+		],
+		display: 'browser',
+		background_color: '#000000',
+		theme_color: '#000000'
+	};
+
+	return new Response(JSON.stringify(manifest), {
+		headers: {
+			'Content-Type': 'application/manifest+json'
+		}
+	});
+};
