0c8e7f842485bab8f26618ab9575c50d42171ebf
James18232 <jjccargill@gmail.com>
2025-07-25 23:05:17 +1000

    Update +server.ts

--- frontend/src/routes/manifest.webmanifest/+server.ts
+++ frontend/src/routes/manifest.webmanifest/+server.ts
@@ -2,8 +2,6 @@ import type { RequestHandler } from './$types';
 import { getAppleIconUrl } from '$lib/utils/apple-icon-util';
 import type { RequestEvent } from '@sveltejs/kit';
 
-export const prerender = true; // this should just prerender manifest.json and not treat it as a server route
-
 export const GET: RequestHandler = async () => {
 	const logoUrl = getAppleIconUrl();
 	const manifest = {
