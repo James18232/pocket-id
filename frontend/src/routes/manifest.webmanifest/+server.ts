7925bb024b8d46c80592ee615face6ec6a2d1cb7
James18232 <jjccargill@gmail.com>
2025-07-26 10:23:36 +1000

    Create +server.ts

---
+++ frontend/src/routes/manifest.webmanifest/+server.ts
@@ -0,0 +1,23 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = ({ url }) => {
+	const isLight = url.searchParams.get('light') !== 'false';
+
+	const manifest = {
+		name: 'PocketID',
+		icons: [
+			{
+        src: `/api/application-configuration/AppleIcon${isLight ? '' : '?light=false'}`,
+				sizes: '180x180',        sizes: '180x180',
+				type: 'image/png'
+			}
+		],
+		display: 'browser'
+	};
+
+	return new Response(JSON.stringify(manifest), {
+		headers: {
+			'Content-Type': 'application/manifest+json'
+		}
+	});
+};
