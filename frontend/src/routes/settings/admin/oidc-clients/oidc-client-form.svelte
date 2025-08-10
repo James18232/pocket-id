70f615b12c107a8e9ce8222e2059cf796b72a1d2
James18232 <jjccargill@gmail.com>
2025-08-10 20:43:38 +1000

    Update oidc-client-form.svelte

--- frontend/src/routes/settings/admin/oidc-clients/oidc-client-form.svelte
+++ frontend/src/routes/settings/admin/oidc-clients/oidc-client-form.svelte
@@ -179,7 +179,7 @@
 		</div>
 	<div class="mt-5 md:col-span-2" transition:slide={{ duration: 200 }}>
         <label class="block mb-2 font-medium">{m.client_id()} ({m.current()})</label>
-		<FormInput value={data.client.id} readonly />
+		<FormInput value={existingClient?.id ?? ''} readonly />
 
 		<label for="replaceClientIdInput" class="block mb-2 font-medium">{m.client_id()} ({m.new()})</label>
 		<FormInput
