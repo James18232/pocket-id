f224f62c5659aad9e62e8208ea6a74d05e544676
James18232 <jjccargill@gmail.com>
2025-08-10 21:45:42 +1000

    Update replace-ClientID.svelte

--- frontend/src/routes/settings/admin/oidc-clients/replace-ClientID.svelte
+++ frontend/src/routes/settings/admin/oidc-clients/replace-ClientID.svelte
@@ -1,31 +1,33 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
+	import Label from '$lib/components/ui/label/label.svelte';
 	import { m } from '$lib/paraglide/messages';
+	import type { OidcClient } from '$lib/types/oidc.type';
+	import FormInput from '$lib/components/form/form-input.svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
 
-	let { data } = $props();
-	let client = $state(data.client);
-
-	let newClientIdInput = $state('');
+	let {
+		client,
+		...restProps
+	}: HTMLAttributes<HTMLDivElement> & {
+		client?: OidcClient;
+	} = $props();
 </script>
 
-<div class="space-y-6">
-	{#if client}
-		<div>
-			<label class="block mb-2 font-medium">{m.client_id()} ({m.current()})</label>
-			<Input value={client.id} readonly />
-		</div>
-		<div>
-			<label for="newClientIdInput" class="block mb-2 font-medium">{m.client_id()} ({m.new()})</label>
-			<Input
-				id="newClientIdInput"
-				bind:value={newClientIdInput}
-				placeholder="Enter new Client ID"
-			/>
-			<Button class="mt-2">Replace Client ID</Button>
-			<!-- Button is not wired yet, since API call is not a focus -->
-		</div>
-	{:else}
-		<div>Loading client...</div>
-	{/if}
+<div {...restProps}>
+	<div>
+		<label class="block mb-2 font-medium">{m.client_id()} ({m.current()})</label>
+		<Input value={client.id} readonly />
+	</div>
+	<div>
+		<label for="newClientIdInput" class="block mb-2 font-medium">{m.client_id()} ({m.new()})</label>
+		<Input
+			id="newClientIdInput"
+			bind:value={newClientIdInput}
+			placeholder="Enter new Client ID"
+		/>
+		<Button class="mt-2">Replace Client ID</Button>
+		<!-- Button is not wired yet, since API call is not a focus -->
+	</div>
 </div>
