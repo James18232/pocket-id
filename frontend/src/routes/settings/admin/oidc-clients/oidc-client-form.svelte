7cac513defb33a9819a90c66b7b1606e39759afe
James18232 <jjccargill@gmail.com>
2025-08-11 11:01:09 +1000

    Update oidc-client-form.svelte

--- frontend/src/routes/settings/admin/oidc-clients/oidc-client-form.svelte
+++ frontend/src/routes/settings/admin/oidc-clients/oidc-client-form.svelte
@@ -179,11 +179,12 @@
 					errors={getFederatedIdentityErrors($errors)}
 				/>
 			</div>
-			<div class="mt-5 md:col-span-2">
+			<!-- <div class="mt-5 md:col-span-2">
 				<EditClientId
 					client={existingClient}
-				/>
+				/> -->
 			</div>
+			<div> test text </div>
 		</div>
 	{/if}
 
