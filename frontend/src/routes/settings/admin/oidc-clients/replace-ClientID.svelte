275992a1fff8f78c95c7494ff2455abb9b5a847b
James18232 <jjccargill@gmail.com>
2025-08-11 16:15:06 +1000

    Update replace-ClientID.svelte

--- frontend/src/routes/settings/admin/oidc-clients/replace-ClientID.svelte
+++ frontend/src/routes/settings/admin/oidc-clients/replace-ClientID.svelte
@@ -22,7 +22,7 @@
 <div {...restProps}>
 	<Button class="mt-0" variant="secondary" size="sm" onclick={() => (showClientInput = !showClientInput)} type="button">
 	<TextCursorInput class="mr-1 size-4" />
-		Replace Client ID or Client secret
+		Replace Client ID / Client secret
 	</Button>
 
 	{#if showClientInput}
