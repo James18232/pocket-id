05378d402f541013e4d27a8c8049bccdd0b8227d
james <james@goldfish.net>
2026-05-30 16:15:36 +1000

    return

--- frontend/src/routes/login/alternative/code/+page.ts
+++ frontend/src/routes/login/alternative/code/+page.ts
@@ -1,13 +1,8 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ url }) => {
-	const redirectParam = url.searchParams.get('redirect') || '/settings';
-
-	const isExternalClient = redirectParam.startsWith('http') && !redirectParam.startsWith(url.origin);
-
 	return {
-	code: url.searchParams.get('code'),
-	redirect: redirectParam,
-	isExternalClient
+		code: url.searchParams.get('code'),
+		redirect: url.searchParams.get('redirect') || '/settings'
 	};
 };
