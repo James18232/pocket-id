e0062799c0086a1223f6860b55b3d785ea25aa04
james <james@goldfish.net>
2026-05-30 15:50:37 +1000

    test

--- frontend/src/routes/login/alternative/code/+page.ts
+++ frontend/src/routes/login/alternative/code/+page.ts
@@ -1,14 +1,13 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ url, fetch }) => {
-    const response = await fetch(`${url.origin}/api/application-configuration`);
-    const configArray = await response.json();
-    
-    const appConfigName = configArray.find((item: any) => item.key === 'appName').value;
+export const load: PageLoad = async ({ url }) => {
+	const redirectParam = url.searchParams.get('redirect') || '/settings';
 
-    return {
-        code: url.searchParams.get('code'),
-        redirect: url.searchParams.get('redirect') || '/settings',
-        appConfigName
-    };
+	const isExternalClient = redirectParam.startsWith('http') && !redirectParam.startsWith(url.origin);
+
+	return {
+	code: url.searchParams.get('code'),
+	redirect: redirectParam,
+	isExternalClient
+	};
 };
