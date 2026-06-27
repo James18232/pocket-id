1e14c1455118dd089f6a7fac2bc7f41ee53c5e93
james <james@goldfish.net>
2026-06-27 16:06:06 +1000

    add arrow into new row for mobile

--- frontend/src/lib/components/code-input.svelte
+++ frontend/src/lib/components/code-input.svelte
@@ -45,14 +45,43 @@
 
 <div
 	class={length > 6
-		? 'relative max-sm:grid max-sm:[grid-template-columns:repeat(8,40px)] max-sm:justify-center max-sm:justify-items-center max-sm:gap-x-0.5 gap-y-3 sm:flex sm:flex-row sm:items-center sm:justify-center sm:gap-x-1.5'
+		? 'relative max-sm:grid max-sm:[grid-template-columns:repeat(8,40px)] max-sm:justify-center max-sm:justify-items-center max-sm:gap-x-0.5 max-sm:gap-y-0 sm:flex sm:flex-row sm:items-center sm:justify-center sm:gap-x-1.5'
 		: 'relative flex flex-nowrap items-center justify-center gap-x-1.5'}
 >
 	{#each chars as char, i}
+		{#if length > 6 && i === 8}
+			<div class="col-span-full hidden max-sm:block w-full h-11 px-2 my-0" aria-hidden="true">
+				<svg
+					class="w-full h-full text-muted-foreground"
+					viewBox="0 0 100 20"
+					preserveAspectRatio="none"
+					fill="none"
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path
+						d="M 98,4 
+							C 98,8 97.5,8 96,8 
+							L 4,8 
+							C 2.5,8 2,8 2,16"
+						stroke-width="0.85"
+					/>
+
+					<polygon
+						points="1,14 2,16 3,14"
+						fill="currentColor"
+						stroke="currentColor"
+						stroke-width="1"
+					/>
+				</svg>
+			</div>
+		{/if}
 		{#if i > 0}
 			{#if length > 6 && i % 4 === 0}
-				<span class="text-muted-foreground hidden text-2xl font-light sm:inline" aria-hidden="true"
-					>–</span
+				<span
+					class={`text-muted-foreground hidden text-2xl font-light sm:inline ${i === 8 ? 'max-sm:hidden' : ''}`}
+					aria-hidden="true">–</span
 				>
 			{:else if length <= 6 && i === Math.floor(length / 2)}
 				<span class="text-muted-foreground text-2xl font-light" aria-hidden="true">–</span>
